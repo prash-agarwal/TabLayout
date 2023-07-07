d3ea875e1a11297e8c3a63347656f5dac6307c43
Prashant Agarwal <prash.agarwal21@gmail.com>
2023-07-07 21:32:24 +0530

    First Commit

---
+++ app/src/main/java/com/example/tablayout/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.tablayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.tablayout.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ViewPagerAdapter adapter=new ViewPagerAdapter(getSupportFragmentManager());
+        binding.viewPager.setAdapter(adapter);
+        binding.tab.setupWithViewPager(binding.viewPager);
+
+    }
+}
